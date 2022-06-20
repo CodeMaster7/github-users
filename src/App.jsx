b6dca027c2484ebc6697814c6d2e8213f69afcfd
sam <sammybonfanti@gmail.com>
2022-06-20 13:50:07 -0700

    feat:

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { Route, Routes } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import Home from './containers/Home'
 
 function App() {
+	const [users, setUsers] = useState([])
+
+	const url = 'https://api.github.com/users?per_page=6'
+
+	const getUsers = async () => {
+		const response = await fetch(url)
+		const FinalData = await response.json()
+		setUsers(FinalData)
+	}
+
+	useEffect(() => {
+		getUsers()
+	}, [])
+
 	return (
 		<>
 			<Routes>
-				<Route path='/' element={<Home />} />
+				<Route path='/' element={<Home users={users}/>} />
 			</Routes>
 		</>
 	)
