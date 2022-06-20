985e001d3241e1e53cd7404f4faa592ddd255686
sam <sammybonfanti@gmail.com>
2022-06-20 13:16:44 -0700

    feat:

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,6 +1,11 @@
+import TopNavbar from "../components/TopNavbar"
+
 function Home() {
   return (
-    <div>Home</div>
+    <>
+        <TopNavbar />
+        <h3 className="text-center mt-5">This is my Home Component</h3>
+    </>
   )
 }
 export default Home
