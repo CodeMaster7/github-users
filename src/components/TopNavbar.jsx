6c7d9f8350b7334ce87414ac386f49039857104f
sam <sammybonfanti@gmail.com>
2022-06-20 13:15:31 -0700

    feat: add navbar component and links

---
+++ src/components/TopNavbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
+function TopNavbar() {
+	return (
+		<Navbar bg='light' variant='light'>
+			<Container>
+				<Navbar.Brand as={Link} to='/'>Github Users</Navbar.Brand>
+				<Nav className='me-auto'>
+					<Nav.Link as={Link} to='/'>Home</Nav.Link>
+				</Nav>
+			</Container>
+		</Navbar>
+	)
+}
+export default TopNavbar
