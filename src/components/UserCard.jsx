4ec4848dd402a83055badfc5945e44b72393cac3
sam <sammybonfanti@gmail.com>
2022-06-20 15:06:39 -0700

    build: add a placeholder image

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -3,14 +3,17 @@ import {Card, Button} from 'react-bootstrap'
 function UserCard() {
   return (
 		<Card className='mx-auto mt-5' style={{ width: '18rem' }}>
-			<Card.Img variant='top' src='holder.js/100px180' />
+			<Card.Img
+				variant='top'
+				src='https://react.semantic-ui.com/images/avatar/large/matthew.png'
+			/>
 			<Card.Body>
 				<Card.Title>Card Title</Card.Title>
 				<Card.Text>
 					Some quick example text to build on the card title and make
 					up the bulk of the card's content.
 				</Card.Text>
-				<Button variant='primary'>View More</Button>
+				<Button variant='primary'>View Profile</Button>
 			</Card.Body>
 		</Card>
   )
