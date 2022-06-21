9eb0d2b82a9793bb39772ed8e8ff8d6e2dba2125
sam <sammybonfanti@gmail.com>
2022-06-20 21:28:05 -0700

    refactor: fetch the github user and pass it to the UserCard component

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,13 +1,66 @@
+import { Form, InputGroup, Button } from 'react-bootstrap'
+import { useState } from 'react'
 import UserCard from '../components/UserCard'
-import TopNavbar from '../components/TopNavbar'
-import Search from '../components/Search'
 
-function Home({ users }) {
+function Home() {
+	const [userInput, setUserInput] = useState('')
+	const [user, setUser] = useState('[]')
+	const [error, setError] = useState('')
+
+	const handleSearch = (e) => {
+		e.preventDefault()
+		setUserInput(e.target.value)
+	}
+
+	const handleSubmit = () => {
+		fetch(`https://api.github.com/users/${userInput}`)
+			.then((res) => res.json())
+			.then((data) => {
+				setUser(data)
+			})
+	}
+
 	return (
 		<>
-			<TopNavbar />
-            <Search />
-			<UserCard />
+			<h1 className='text-center mt-5'>Github Users</h1>
+			{user.login ? (
+				<>
+					<Form className='w-50 mx-auto mt-2 d-flex'>
+						<InputGroup size='lg'>
+							<Form.Control
+								placeholder='Search Github User'
+								aria-label='Large'
+								aria-describedby='inputGroup-sizing-sm'
+								onChange={handleSearch}
+							/>
+						</InputGroup>
+						<Button
+							className='ms-3'
+							variant='primary'
+							onClick={handleSubmit}>
+							Search
+						</Button>
+					</Form>
+					<UserCard user={user} />
+				</>
+			) : (
+				<Form className='w-50 mx-auto mt-2 d-flex'>
+					<InputGroup size='lg'>
+						<Form.Control
+							placeholder='Search Github User'
+							aria-label='Large'
+							aria-describedby='inputGroup-sizing-sm'
+							onChange={handleSearch}
+						/>
+					</InputGroup>
+					<Button
+						className='ms-3'
+						variant='primary'
+						onClick={handleSubmit}>
+						Search
+					</Button>
+				</Form>
+			)}
 		</>
 	)
 }
